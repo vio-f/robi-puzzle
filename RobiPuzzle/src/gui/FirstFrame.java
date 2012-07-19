d058a614b76f7ab99db5b18484cc4a3d8510eac4
haidu.adi@gmail.com <haidu.adi@gmail.com@f2f62bce-d391-8329-57ea-12a414c88187>
2012-07-19 18:57:14 +0000

    Initial commit

---
+++ RobiPuzzle/src/gui/FirstFrame.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ * TODO DESCRIPTION
+ * 
+ * @author Adrian.Haidu
+ */
+public class FirstFrame extends JFrame {
+  
+  
+  /**
+   * Constructs a new instance.
+   */
+  public FirstFrame() {
+    
+  }
+
+}
