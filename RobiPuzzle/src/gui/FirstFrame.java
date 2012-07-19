86e17fbe8932629c3c7df904207cc819917ad35a
haidu.adi@gmail.com <haidu.adi@gmail.com@f2f62bce-d391-8329-57ea-12a414c88187>
2012-07-19 19:45:42 +0000

    Content management test

--- RobiPuzzle/src/gui/FirstFrame.java
+++ RobiPuzzle/src/gui/FirstFrame.java
@@ -1,8 +1,15 @@
 package gui;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 /**
  * The first JFrame displayed when the application starts
@@ -12,6 +19,8 @@ import javax.swing.JFrame;
 public class FirstFrame extends JFrame {
   /** initialDimension */
   private Dimension initialDimension = new Dimension(640, 480);
+  /** buttonList */
+  private ArrayList<JButton> buttonList = new ArrayList<JButton>();
   
   /**
    * Constructs a new instance.
@@ -20,7 +29,8 @@ public class FirstFrame extends JFrame {
     super();
     this.setVisible(true);
     this.configureFrame();
-       
+    this.addComponents();
+    this.changeText();
     pack();
   }
   
@@ -30,7 +40,48 @@ public class FirstFrame extends JFrame {
   private void configureFrame() {
     this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     this.setPreferredSize(this.initialDimension);
+    BorderLayout borderLayout = new BorderLayout();
+    this.setLayout(borderLayout);
+  }
+  
+  /**
+   * Populate the frame with content
+   */
+  private void addComponents() {
+    JPanel northPanel = new JPanel();
+    JPanel southPanel = new JPanel();
+    //add panels to appropriate location
+    this.add(northPanel, BorderLayout.NORTH);
+    this.add(southPanel, BorderLayout.SOUTH);
     
+    for (int i = 0; i < 40; i++) {
+      JButton but = new JButton(i + "");
+      //set destination
+      if (i%2==0){
+      northPanel.add(but);
+      }
+      else {
+        southPanel.add(but);
+      }
+      this.buttonList.add(but);
+          }
+  }
+  
+  /**
+   * Change the text of all buttons
+   */
+  private void changeText() {
+    try {
+      Thread.sleep(5*1000);
+    } catch (InterruptedException e) {
+      // TODO Add your own exception handling here, consider logging
+      e.printStackTrace();
+    }
+    for (int i = 0; i < this.buttonList.size(); i++) {
+      JButton jb = this.buttonList.get(i);
+      jb.setText("New text " + i);
+      
+    }
   }
 
 }
