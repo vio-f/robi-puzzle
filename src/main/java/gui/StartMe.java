6e3b447b2c78731e55d073b5ab2fabcbc953c97b
viorel.florian <viorel.florian@gmail.com>
2012-07-23 23:33:52 +0000

    update

---
+++ src/main/java/gui/StartMe.java
@@ -0,0 +1,15 @@
+package gui;
+
+/**
+ * TODO DESCRIPTION
+ * 
+ * @author Adrian.Haidu
+ */
+public class StartMe {
+  
+  
+  public static void main(String[] args) {
+    new FirstFrame();
+  }
+
+}
