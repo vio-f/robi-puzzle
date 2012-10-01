75095a5211e99a132746a751320a53b6760ecd74
viorel.florian <viorel.florian@gmail.com>
2012-10-01 00:33:28 +0000

    added support for populating containers

--- src/main/java/gui/AbstractPuzzlePiecesPanel.java
+++ src/main/java/gui/AbstractPuzzlePiecesPanel.java
@@ -1,11 +1,17 @@
 package gui;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.LayoutManager;
+import java.awt.image.BufferedImage;
 import java.util.LinkedList;
 
-import javax.swing.JButton;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import util.ImageUtil;
 
 /**
  * TODO DESCRIPTION
@@ -19,17 +25,25 @@ public abstract class AbstractPuzzlePiecesPanel extends JPanel {
 
   // TODO make this dynamic
   /** defaultLayout */
-  private GridLayout defaultLayout = new GridLayout(5, 4);
+  private GridLayout layout = null;
 
   /** puzzlePieces */
-  private LinkedList<JButton> puzzlePiecesContainerList = new LinkedList<JButton>();
+  private LinkedList<JLabel> puzzlePiecesContainerList = new LinkedList<JLabel>();
+  
+  /** rows */
+  int rows;
+  /** columns */
+  int columns;
 
+  /** puzzlePieces */
+  private BufferedImage[] puzzlePieces;
+  
   /**
    * Constructs a new instance.
    */
   public AbstractPuzzlePiecesPanel() {
     super();
-    this.setLayout(this.defaultLayout);
+   // this.setLayout(this.defaultLayout);
   }
 
   /**
@@ -61,24 +75,54 @@ public abstract class AbstractPuzzlePiecesPanel extends JPanel {
   }
 
   /**
-   * Add puzzle pieces to the panel
+   * Constructs a new instance.
+   * @param panelDimensions
    */
-  protected void addPuzzleContainers() {
-    // TODO make this configurable
-    for (int i = 0; i < 20; i++) {
-      JButton but = new JButton("Test");
-      this.puzzlePiecesContainerList.add(but);
-      this.add(but);
-
-    }
+  public AbstractPuzzlePiecesPanel(Dimension panelDimensions) {
+    this.setSize(panelDimensions);
   }
 
+  /**
+   * Add puzzle pieces to the panel
+   * @param rows 
+   * @param cols 
+   */
+  public void addPuzzleContainersAndPopulate(int rows, int cols) {
+    this.rows = rows;
+    this.columns = cols;
+    this.puzzlePieces = this.getPuzzlePieces();
+    this.layout = new GridLayout(rows, cols, 5, 5);
+    this.setLayout(this.layout);
+     for (int i = 0; i < rows*cols; i++) {
+     JLabel label = new JLabel();
+     
+     Image image = this.puzzlePieces[i];
+     ImageIcon imageIcon = new ImageIcon(image);
+     label.setIcon(imageIcon);
+     //label.getGraphics().drawImage(this.puzzlePieces[i], 0, 0, null);
+     this.puzzlePiecesContainerList.add(label);
+     this.add(label);
+     }
+  }
+  
+  
+
+  /**
+   * Create an array of puzzle pieces
+   * @return
+   */
+  private BufferedImage[] getPuzzlePieces() {
+    //String imagePath = "d:/_Work/_JavaWorkspace/samples/mario2.png";
+    String imagePath = "resources/ic_board.jpg";
+    return ImageUtil.splitImage(ImageUtil.loadImageFromResources(imagePath), this.rows, this.columns);
+  }
+  
   /**
    * Set puzzlePiecesContainer.
    * 
    * @param puzzlePiecesContainer
    */
-  public void setPuzzlePiecesContainerList(LinkedList<JButton> puzzlePiecesContainer) {
+  public void setPuzzlePiecesContainerList(LinkedList<JLabel> puzzlePiecesContainer) {
     this.puzzlePiecesContainerList = puzzlePiecesContainer;
   }
 
@@ -87,8 +131,8 @@ public abstract class AbstractPuzzlePiecesPanel extends JPanel {
    * 
    * @return puzzlePiecesContainer
    */
-  public LinkedList<JButton> getPuzzlePiecesContainerList() {
-    return puzzlePiecesContainerList;
+  public LinkedList<JLabel> getPuzzlePiecesContainerList() {
+    return this.puzzlePiecesContainerList;
   }
 
 }// EOF
