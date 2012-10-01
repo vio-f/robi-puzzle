75eca2db812dcd1800bf593dc2656b73e32c51d6
viorel.florian@gmail.com <viorel.florian@gmail.com@f2f62bce-d391-8329-57ea-12a414c88187>
2012-10-01 20:53:48 +0000

    Test classes

---
+++ trunk/src/main/java/test/animation/StartAnimationGui.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright Viorel Florian
+ * All rights reserved.
+ * viorel.florian@gmail.com
+ */
+package test.animation;
+
+/**
+ * TODO DESCRIPTION
+ * 
+ * @author viorel.florian
+ */
+public class StartAnimationGui {
+  
+  public static void main(String[] args) {
+    AnimationJFrame animFrame = new AnimationJFrame();
+    animFrame.setVisible(true);
+    
+  }
+
+}
